3b41b2aa429218d50c8bcd22a5336025760a0bb3
Nᴏᴛʜɪɴɢ <125535875+nothing022@users.noreply.github.com>
2023-09-22 11:18:00 +0530

    Update settings.py

--- AloneXMusic/plugins/bot/settings.py
+++ AloneXMusic/plugins/bot/settings.py
@@ -55,7 +55,7 @@ async def settings_mar(client, message: Message, _):
 @languageCB
 async def gib_repo(client, CallbackQuery, _):
     await CallbackQuery.edit_message_media(
-        InputMediaVideo("https://te.legra.ph/file/e6471d19bd04a5095436a.mp4"),
+        InputMediaVideo("https://te.legra.ph/file/1e8cfe0e05b2343848f65.mp4"),
         reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton(text="ʙᴀᴄᴋ", callback_data=f"settingsback_helper")]]
         ),
